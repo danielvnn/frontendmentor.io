c106fcf6c9bc1bc59c97c43dd4f782d3bd9b1c23
danielervanda <danielervanda0@gmail.com>
2023-03-24 14:27:13 +0700

    finish javascript

--- results-summary-component/scripts.js
+++ results-summary-component/scripts.js
@@ -1,3 +1,27 @@
-fetch('data.json')
+fetch(
+	'https://danielervanda.github.io/frontendmentor.io/results-summary-component/data.json'
+)
 	.then((response) => response.json())
-	.then((data) => console.log(data))
+	.then((data) => {
+		summary = data
+		summary.map((data) => {
+			let body = document.getElementsByClassName('summary__body')[0]
+			body.innerHTML += `<div class="summary__item summary__item--${data.category.toLowerCase()}">
+            <div class="summary__item-name">
+              <img
+                src="${data.icon}"
+                alt=""
+                class="summary__item-icon"
+              />
+              <p class="summary__item-text">${data.category}</p>
+            </div>
+            <div class="summary__score">
+              <p class="summary__score-value">
+                <span class="summary__item-value--bold">${
+					data.score
+				}</span> / 100
+              </p>
+            </div>
+          </div>`
+		})
+	})
