8014c3da612f12949c72c6e08faaf945593981de
danielervanda <danielervanda0@gmail.com>
2023-03-30 12:55:59 +0700

    fix share button if browser width is changed

--- article-preview-component/scripts.js
+++ article-preview-component/scripts.js
@@ -4,9 +4,25 @@ const mainbox = document.querySelector('.article__author')
 const avatar = document.querySelector('.article__author-avatar')
 const details = document.querySelector('.article__details')
 
+window.addEventListener('resize', function () {
+	const width = window.innerWidth
+	if (width < 576 && shareBox.style.display === 'flex') {
+		mainbox.classList.remove('article__author')
+		mainbox.classList.add('article__author--mobile')
+		avatar.style.display = 'none'
+		details.style.display = 'none'
+	} else if (width >= 577 && details.style.display === 'none') {
+		avatar.style.display = 'block'
+		details.style.display = 'flex'
+		mainbox.classList.add('article__author')
+		mainbox.classList.remove('article__author--mobile')
+	}
+})
+
 function getSize() {
 	return screen.width
 }
+
 shareIcon.addEventListener('click', () => {
 	let widthsize = getSize()
 
