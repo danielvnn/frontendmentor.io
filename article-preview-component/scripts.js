515737d11f82a74bca9483383fc745a3685f734a
danielervanda <danielervanda0@gmail.com>
2023-03-30 12:18:44 +0700

    add more css & javascript for share button

--- article-preview-component/scripts.js
+++ article-preview-component/scripts.js
@@ -1,10 +1,30 @@
 const shareIcon = document.querySelector('.share__icon')
 const shareBox = document.querySelector('.share__box')
+const mainbox = document.querySelector('.article__author')
+const avatar = document.querySelector('.article__author-avatar')
+const details = document.querySelector('.article__details')
 
+function getSize() {
+	return screen.width
+}
 shareIcon.addEventListener('click', () => {
-	if (shareBox.style.display === 'flex') {
-		shareBox.style.display = 'none'
-	} else {
+	let widthsize = getSize()
+
+	if (shareBox.style.display === 'flex' && widthsize <= 576) {
+		shareBox.style.display = ''
+		avatar.style.display = 'block'
+		details.style.display = 'flex'
+		mainbox.classList.add('article__author')
+		mainbox.classList.remove('article__author--mobile')
+	} else if (shareBox.style.display === '' && widthsize < 576) {
+		shareBox.style.display = 'flex'
+		mainbox.classList.remove('article__author')
+		mainbox.classList.add('article__author--mobile')
+		avatar.style.display = 'none'
+		details.style.display = 'none'
+	} else if (shareBox.style.display === 'flex' && widthsize > 576) {
+		shareBox.style.display = ''
+	} else if (shareBox.style.display === '' && widthsize > 576) {
 		shareBox.style.display = 'flex'
 	}
 })
